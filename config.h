fb62253761dd0cdba90330bb793c6c5a69699d8e
Lionel Pinkhard <lionelpinkhard@me.com>
2020-12-15 20:25:52 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define GFX_MODE GFX_AUTODETECT_WINDOWED
+#define GFX_HEIGHT 640
+#define GFX_WIDTH 480
+
+#define GAME_DATAFILE "game.dat"
+
+#define COL_LANDSCAPE makecol(153, 76, 0)
+#define COL_SKY makecol(135, 206, 235)
+
+#endif
---
+++ defines.h
@@ -0,0 +1,11 @@
+/* Allegro datafile object indexes, produced by dat v4.4.2, MinGW32 */
+/* Datafile: game.dat */
+/* Date: Tue Jan 15 14:42:54 2019 */
+/* Do not hand edit! */
+
+#define CANNON_WAV                       0        /* SAMP */
+#define ENEMY_BMP                        1        /* BMP  */
+#define FONT_PCX                         2        /* FONT */
+#define MUSIC_MID                        3        /* MIDI */
+#define PLAYER_BMP                       4        /* BMP  */
+
